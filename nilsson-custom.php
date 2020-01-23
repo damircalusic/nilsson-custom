0cf2d01bf1c821d3257cb6233d7bd22d3579c45c
Damir Calusic <damir@damircalusic.com>
2020-01-23 23:04:19 +0100

    Modified and simplified the initial code

--- nilsson-custom.php
+++ nilsson-custom.php
@@ -19,30 +19,32 @@ if(!defined('ABSPATH')) exit;
  * Filters the default archive titles.
  */
 function nilcus_get_the_archive_title() {
-	if ( is_category() ) {
-		$title = '<span class="page-description">' . single_term_title( '', false ) . '</span>';
-	} elseif ( is_tag() ) {
-		$title = '<span class="page-description">' . single_term_title( '', false ) . '</span>';
+	if ( is_category() || is_tag() ) {
+		$title = single_term_title('', false);
 	} elseif ( is_author() ) {
-		$title = '<span class="page-description">' . get_the_author_meta( 'display_name' ) . '</span>';
+		$title = get_the_author_meta( 'display_name' );
 	} elseif ( is_year() ) {
-		$title = '<span class="page-description">' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentynineteen' ) ) . '</span>';
+		$title = get_the_date( _x( 'Y', 'yearly archives date format', 'twentynineteen' ) );
 	} elseif ( is_month() ) {
-		$title = '<span class="page-description">' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentynineteen' ) ) . '</span>';
+		$title = get_the_date( _x( 'F Y', 'monthly archives date format', 'twentynineteen' ) );
 	} elseif ( is_day() ) {
-		$title = '<span class="page-description">' . get_the_date() . '</span>';
+		$title = get_the_date();
 	} elseif ( is_post_type_archive() ) {
-		$title = '<span class="page-description">' . post_type_archive_title( '', false ) . '</span>';
+		$title = post_type_archive_title( '', false );
 	} elseif ( is_tax() ) {
 		$tax = get_taxonomy(get_queried_object()->taxonomy);
 		$title = $tax->labels->singular_name;
-    } 
+    	} 
 	
-	return $title.'<blockquote class="wp-block-quote">'.get_the_archive_description().'</blockquote>';
+	return '<span class="page-description">'.$title.'</span> <blockquote class="wp-block-quote">'.get_the_archive_description().'</blockquote>';
 }
 
+/**
+ * Functions that run on INIT of website
+ */
 function nilcus_init(){
 	add_filter('get_the_archive_title', 'nilcus_get_the_archive_title');
 }
 
+// Add Actions
 add_action('init', 'nilcus_init', 99);
